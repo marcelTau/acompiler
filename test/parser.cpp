bd9139807bf81014072ac65ea0503addb2dce104
marcel <marcel.taubert@epitech.eu>
2022-08-27 13:13:56 +0200

    Add mixed test

--- test/parser.cpp
+++ test/parser.cpp
@@ -139,7 +139,31 @@ TEST(parser, variable_assignment_with_recurring_plus_expressions) {
     }
 }
 
+TEST(parser, variable_assignment_with_recurring_mixed_plus_minus_expressions) {
+    Scanner s;
+    Parser p;
+    auto tokens = s.scan("let a = 10 + 20 - 30;");
+    auto stmts = p.parse(tokens);
 
+    Parser::StatementList expected;
+
+    auto lhs = std::make_unique<Expressions::Number>("10");
+    Token token = Token({ .type = TokenType::Plus, .lexeme = "+", .position = { .line = 1, .column = 12 } });
+    auto rhs = std::make_unique<Expressions::Number>("20");
+    Token token2 = Token({ .type = TokenType::Minus, .lexeme = "-", .position = { .line = 1, .column = 17 } });
+    auto rrhs = std::make_unique<Expressions::Number>("30");
+
+    auto initializer = std::make_unique<Expressions::BinaryOperator>(std::move(lhs), token, std::move(rhs));
+    auto initializer2 = std::make_unique<Expressions::BinaryOperator>(std::move(initializer), token2, std::move(rrhs));
+
+    expected.push_back(std::make_unique<Statements::VariableDefinition>("a", std::move(initializer2)));
+
+    EXPECT_TRUE(is_same(stmts, expected));
+
+    if (HasFailure()) {
+        fmt::print(stderr, "#{} {}#", stmts, expected);
+    }
+}
 
 
 
