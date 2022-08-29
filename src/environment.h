891504c045a2ced823fb4e9bb7a14f650a2d2350
marcel <marcel.taubert@epitech.eu>
2022-08-29 15:05:45 +0200

    Add environment class for variable scope

---
+++ src/environment.h
@@ -0,0 +1,73 @@
+#pragma once
+
+#include "datatype.h"
+#include "token.h"
+#include <spdlog/spdlog.h>
+#include <variant>
+#include <cassert>
+
+using Value = std::variant<int, bool>;
+
+struct Object {
+    DataType type;
+    Value value;
+};
+
+struct Environment {
+
+    Environment() = default;
+    Environment(Environment *enclosing_env)
+        : enclosing { enclosing_env }
+    {
+    }
+            
+    void define(const std::string& name, const Object& value) {
+        values[name] = value;
+    }
+
+    Object getAt(std::size_t distance, const std::string& name) {
+        if (distance == 0) {
+            try {
+                return values.at(name);
+            } catch (std::out_of_range& e) {
+                assert(false && "TODO: Environment::getAt");
+            }
+        }
+        assert(enclosing);
+        return enclosing->getAt(distance - 1, name);
+    }
+
+    Object get(const Token& name) {
+        try {
+            return values.at(name.lexeme);
+        } catch (std::out_of_range&) {
+            assert(false && "TODO: Environment::get()");
+        }
+        assert(false);
+        return Object{};
+    }
+
+    void assign(const Token& name, const Object& value) {
+        if (values.contains(name.lexeme)) {
+            values[name.lexeme] = value;
+        } else if (enclosing != nullptr) {
+            enclosing->assign(name, value);
+        } else {
+            spdlog::error(fmt::format("Undefined variable at '{}'", name));
+            assert(false);
+        }
+    }
+
+    void assignAt(std::size_t distance, const Token& name, const Object& value) {
+        if (distance == 0) {
+            values.insert({ name.lexeme, value });
+        } else {
+            assert(enclosing);
+            enclosing->assignAt(distance - 1, name, value);
+        }
+    }
+
+private:
+    std::unordered_map<std::string_view, Object> values;
+    Environment *enclosing { nullptr };
+};
